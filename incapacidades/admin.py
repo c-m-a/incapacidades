2bf4ef6b5e63112f29348ee68b0b11592746dc18
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-05-02 15:26:21 -0500

    Fix: Corrigiendo error en el nombre de la tablas en la anterior migracion

--- incapacidades/admin.py
+++ incapacidades/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Afp, CentroCosto, ClaseIncapacidad, Concepto,  Diagnostico, Eps, Empleado, EstadoIncapacidad, FechaDistribucion, Movimiento
 
-# Register your models here.
+admin.site.register(Afp)
+admin.site.register(CentroCosto)
+admin.site.register(ClaseIncapacidad)
+admin.site.register(Concepto)
+admin.site.register(Diagnostico)
+admin.site.register(Eps)
+admin.site.register(Empleado)
+admin.site.register(EstadoIncapacidad)
+admin.site.register(FechaDistribucion)
+admin.site.register(Movimiento)
+
