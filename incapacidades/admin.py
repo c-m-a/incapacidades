1dc2a05df5b2fa56ee0073362dbc2180d23faac7
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-05-03 07:21:25 -0500

    Add: Agregado campo de busqueda en algunos modelos en modulo de Administracion

--- incapacidades/admin.py
+++ incapacidades/admin.py
@@ -1,14 +1,41 @@
 from django.contrib import admin
 from .models import Afp, CentroCosto, ClaseIncapacidad, Concepto,  Diagnostico, Eps, Empleado, EstadoIncapacidad, FechaDistribucion, Movimiento
 
-admin.site.register(Afp)
-admin.site.register(CentroCosto)
-admin.site.register(ClaseIncapacidad)
-admin.site.register(Concepto)
-admin.site.register(Diagnostico)
-admin.site.register(Eps)
-admin.site.register(Empleado)
-admin.site.register(EstadoIncapacidad)
+class AfpAdmin(admin.ModelAdmin):
+    search_fields = ['codigo', 'nit', 'nombre']
+
+class CentroCostoAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+
+class ClaseIncapacidadAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+
+class ConceptoAdmin(admin.ModelAdmin):
+    search_fields = ['codigo', 'nombre']
+
+class DiagnosticoAdmin(admin.ModelAdmin):
+    search_fields = ['codigo', 'nombre']
+
+class EpsAdmin(admin.ModelAdmin):
+    search_fields = ['codigo', 'nit', 'nombre']
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    search_fields = ['docto_empleado', 'nombre']
+
+class EstadoIncapacidadAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+
+class MovimientoAdmin(admin.ModelAdmin):
+    search_fields = ['cod_incapacidad', 'serie']
+
+admin.site.register(Afp, AfpAdmin)
+admin.site.register(CentroCosto, CentroCostoAdmin)
+admin.site.register(ClaseIncapacidad, ClaseIncapacidadAdmin)
+admin.site.register(Concepto, ConceptoAdmin)
+admin.site.register(Diagnostico, DiagnosticoAdmin)
+admin.site.register(Eps, EpsAdmin)
+admin.site.register(Empleado, EmpleadoAdmin)
+admin.site.register(EstadoIncapacidad, EstadoIncapacidadAdmin)
 admin.site.register(FechaDistribucion)
-admin.site.register(Movimiento)
+admin.site.register(Movimiento, MovimientoAdmin)
 
