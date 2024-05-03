20e7161e13f1ffa077ec22f62d4eef182014c5dd
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-05-03 08:18:01 -0500

    Add: Agregando campos y filtros cada modelo en la Administracion

--- incapacidades/admin.py
+++ incapacidades/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Afp, CentroCosto, ClaseIncapacidad, Concepto,  Diagnostico, Eps, Empleado, EstadoIncapacidad, FechaDistribucion, Movimiento
 
 class AfpAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nit', 'nombre')
     search_fields = ['codigo', 'nit', 'nombre']
 
 class CentroCostoAdmin(admin.ModelAdmin):
@@ -11,21 +12,35 @@ class ClaseIncapacidadAdmin(admin.ModelAdmin):
     search_fields = ['nombre']
 
 class ConceptoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre')
     search_fields = ['codigo', 'nombre']
 
 class DiagnosticoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre')
     search_fields = ['codigo', 'nombre']
 
 class EpsAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nit', 'nombre')
     search_fields = ['codigo', 'nit', 'nombre']
 
 class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('docto_empleado', 'nombre', 'estado', 'eps', 'afp')
+    list_filter = ('genero', 'estado', 'eps', 'afp')
     search_fields = ['docto_empleado', 'nombre']
 
 class EstadoIncapacidadAdmin(admin.ModelAdmin):
     search_fields = ['nombre']
 
 class MovimientoAdmin(admin.ModelAdmin):
+    list_display = (
+        'cod_incapacidad',
+        'serie',
+        'empleado',
+        'diagnostico',
+        'centro_costo',
+        'concepto',
+        'incapacidad'
+    )
     search_fields = ['cod_incapacidad', 'serie']
 
 admin.site.register(Afp, AfpAdmin)
