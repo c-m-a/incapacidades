314f22547a413f80dc70c8645711e963b910b012
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-05-12 18:38:48 -0500

    Refactor: Cambiando end points de personas a empleados

--- incapacidades/urls.py
+++ incapacidades/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('agregar-movimiento/', views.agregar_movimiento, name='agregar_movimiento'),
     path('b/conceptos/', views.buscar_conceptos, name='buscar_conceptos'),
     path('b/diagnosticos/', views.buscar_diagnosticos, name='buscar_diagnosticos'),
-    path('b/personas/', views.buscar_personas, name='buscar_persona'),
-    path('b/personas/movimientos/', views.buscar_personas_movimientos, name='buscar_personas_movimientos'),
+
+    # Empleados
+    path('empleados/<int:id>/', views.empleado_detalles, name='empleado_detalles'),
+    path('b/empleados/', views.buscar_personas, name='buscar_persona'),
+    path('b/empleados/movimientos/', views.buscar_personas_movimientos, name='buscar_personas_movimientos'),
 ]
